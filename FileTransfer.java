4376e45609675d193974564314d5f6cf7c5da8d2
jcarreracpp <jcarrera@cpp.edu>
2017-05-31 23:52:08 -0700

    ARGH THIS IS PAIN, WHAT ARE KEYS, WHAT IS AES, WHAT IS RSA, JUST WORK TOGETHER YOU TWO, DAMNIT.

--- FileTransfer.java
+++ FileTransfer.java
@@ -13,7 +13,11 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Scanner;
+import java.util.zip.CRC32;
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 
 
@@ -24,9 +28,20 @@ import javax.crypto.Cipher;
 public class FileTransfer {
 public static void main(String[] args) throws Exception {
     boolean disconnected = false;
+    boolean takingInput = true;
+    byte[] retrievedFile;
+    String file;
+    long size = 0;
+    int chunkSize;
+    Key key = null;
+    Key clientKey = null;
+    int expectedSeqNum = 0;
+    ObjectOutputStream oo;
+    ObjectInputStream oi;
+    
     
         //Keygen
-        //if (args.length > 0 && args[0].equals("makekeys")) {
+        if (args.length > 0 && args[0].equals("makekeys")) {
             try {
                 KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
                 gen.initialize(4096);
@@ -39,53 +54,96 @@ public static void main(String[] args) throws Exception {
                 try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File("private.bin")))) {
                     oos.writeObject(privateKey);
                 }
+                System.out.println("Keys genned.");
             } catch (NoSuchAlgorithmException | IOException e) {
                 e.printStackTrace(System.err);
             }
-        //}
+        }
         
         //Server mode
         if (args.length > 0 && args[0].equals("server")) {
         String privateKeyFile = args[1];
         int port = Integer.parseInt(args[2]);
+        
         PrivateKey privateKey;
-        ObjectInputStream prois = new ObjectInputStream(new FileInputStream("private.bin"));        
+        ObjectInputStream prois = new ObjectInputStream(new FileInputStream(privateKeyFile));        
 
         while (!disconnected) {
+
             try (ServerSocket serverMode = new ServerSocket(port)) {
                 Socket socket = serverMode.accept();
-                ObjectOutputStream oo = new ObjectOutputStream(socket.getOutputStream());
-                ObjectInputStream oi = new ObjectInputStream(socket.getInputStream());
+                oo = new ObjectOutputStream(socket.getOutputStream());
+                oi = new ObjectInputStream(socket.getInputStream());
                 
-                try{
-                DisconnectMessage response = (DisconnectMessage)oi.readObject();
+                while(takingInput){
+                Message response = (Message)oi.readObject();
+                
+                //Disconect message response
+                if(response.getType() == MessageType.DISCONNECT){
                 socket.close();
                 serverMode.close();
-                } catch(ClassCastException e){
-                    System.out.println("DM "+e);
                 }
                 
-                try{
-                StartMessage response = (StartMessage)oi.readObject();
-                String file = response.getFile();
-                long size = response.getSize();
-                int chunkSize = response.getChunkSize();
-                byte[] encryptedKey = response.getEncryptedKey();
+                //Stop message response
+                if(response.getType() == MessageType.STOP){
+                    AckMessage abort = new AckMessage(-1);
+                    oo.writeObject(abort);
+                }
+                
+                //Start message response
+                if(response.getType() == MessageType.START){
+                //StartMessage response = (StartMessage)oi.readObject();
+                StartMessage startMessage = (StartMessage)response;
+                file = startMessage.getFile();
+                size = startMessage.getSize();
+                retrievedFile = new byte[(int)size];
+                chunkSize = startMessage.getChunkSize();
+                byte[] encryptedKey = startMessage.getEncryptedKey();
                 
                     privateKey = (PrivateKey) prois.readObject();               
                 
                 Cipher c = Cipher.getInstance("AES");
                 c.init(Cipher.UNWRAP_MODE, privateKey);
-                Key key = (Key) c.unwrap(encryptedKey, "AES", ?);
+                key = (Key) c.unwrap(encryptedKey, "AES", Cipher.PUBLIC_KEY);
                 
                     try{
                         AckMessage accept = new AckMessage(0);
                         oo.writeObject(accept);
                     }catch (ClassCastException e){}
-                    
-                } catch(ClassCastException e){
-                    System.out.println("StaM "+e);
                 }
+                
+                //Chunk message response
+                if(response.getType() == MessageType.CHUNK){
+                    Chunk chunk = (Chunk) response;
+                    if(chunk.getSeq() == expectedSeqNum){
+                        
+                        Cipher c = Cipher.getInstance("AES");
+                        c.init(Cipher.DECRYPT_MODE, key);
+                        
+                        CRC32 crc = new CRC32();
+                        crc.update(chunk.getData(), 0, chunk.getData().length);
+                        crc.getValue();
+                        if(chunk.getCrc() == crc.getValue()){
+                            System.out.println("Chunk received [" + expectedSeqNum + "/" + size + "]");
+                            expectedSeqNum++;
+                            //retrievedFile[expectedSeqNum] = crc.getValue();
+                            AckMessage next = new AckMessage(expectedSeqNum);
+                            oo.writeObject(next);
+                            if(size == expectedSeqNum){
+                                System.out.println("File transferred, shutting down...");
+                                takingInput = false;
+                                disconnected = true;
+                                socket.close();
+                                serverMode.close();
+                            }
+                        }
+                        
+                    }else{
+                        AckMessage abort = new AckMessage(expectedSeqNum);
+                        oo.writeObject(abort);
+                    }
+                }
+            }
             }
         }
         }
@@ -95,6 +153,36 @@ public static void main(String[] args) throws Exception {
             String publicKeyFile = args[1];
             String host = args[2];
             String port = args[3];
+            
+            Socket cSocket = new Socket(host, Integer.parseInt(port));
+            
+        ObjectInputStream puois = new ObjectInputStream(new FileInputStream(publicKeyFile));
+            
+            PublicKey publicKey = (PublicKey) puois.readObject();
+            oo = new ObjectOutputStream(cSocket.getOutputStream());
+            oi = new ObjectInputStream(cSocket.getInputStream());
+                
+            Scanner keyboardIn = new Scanner(System.in);
+            System.out.print("Enter filepath: ");
+            String filepath = keyboardIn.nextLine();
+            System.out.print("Enter chunk size: ");
+            String specifiedChunkSize = keyboardIn.nextLine();
+            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+            keyGen.init(128);
+            SecretKey sessionKey = keyGen.generateKey();
+            Cipher c = Cipher.getInstance("AES");
+            c.init(Cipher.WRAP_MODE, publicKey);
+            byte[] toSend = c.wrap(sessionKey);
+            
+            StartMessage begin = new StartMessage(filepath, toSend, Integer.parseInt(specifiedChunkSize));
+            
+            oo.writeObject(begin);
+            Message received = (Message)oi.readObject();
+            
+            if(received.getType() == MessageType.ACK){
+                System.out.println("SUCCESS");
+            }
+            
         }
     }
 }
